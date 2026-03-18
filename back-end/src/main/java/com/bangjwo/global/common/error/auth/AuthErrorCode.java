980fa97415c2b2bb4533786d4fdbbb0f0918c2f9
황인준 <os2864@naver.com>
2026-03-18 11:48:05 +0900

    fix: 잘못된 예외 응답코드 수정

--- back-end/src/main/java/com/bangjwo/global/common/error/auth/AuthErrorCode.java
+++ back-end/src/main/java/com/bangjwo/global/common/error/auth/AuthErrorCode.java
@@ -11,7 +11,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
 	NOT_EXIST_AUTHORIZATION_TOKEN(HttpStatus.FORBIDDEN, "AUTH4001", "로그인 정보가 없습니다."),
-	INVALID_AUTHORIZATION_TOKEN(HttpStatus.NOT_FOUND, "AUTH4002", "잘못된 인증 토큰 정보입니다."),
+	INVALID_AUTHORIZATION_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH4002", "잘못된 인증 토큰 정보입니다."),
 	MEMBER_JSON_SERIALIZE_FAIL(HttpStatus.SERVICE_UNAVAILABLE, "AUTH4003", "유저 정보 직렬화에 실패하였습니다"),
 	INVALID_AUTH_CODE(HttpStatus.BAD_REQUEST, "AUTH4004", "redirectUrl에 유효한 인가 코드(code)가 없습니다."),
 	KAKAO_TOKEN_REQUEST_FAILED(HttpStatus.BAD_GATEWAY, "AUTH5001", "카카오 토큰 요청에 실패하였습니다."),
