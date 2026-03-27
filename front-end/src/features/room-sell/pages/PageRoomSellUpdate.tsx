7720c97250dd86637b080fb9aa765ff3034e8646
황인준 <os2864@naver.com>
2026-03-27 23:52:40 +0900

    feat: 매물 수정 함수 추가

--- front-end/src/features/room-sell/pages/PageRoomSellUpdate.tsx
+++ front-end/src/features/room-sell/pages/PageRoomSellUpdate.tsx
@@ -12,7 +12,10 @@ const PageRoomSellUpdate = () => {
     <LayoutTitle title="내놓은 집 수정하기">
       <RoomForm
         type="update"
+        roomId={roomId}
         onSubmit={(_, __, ___, roomLatLng) => {
+          alert("매물이 성공적으로 수정되었습니다.");
+
           if (roomLatLng && roomId !== null) {
             const { lat, lng } = roomLatLng;
             goToRoomDetail(lat, lng, roomId);
