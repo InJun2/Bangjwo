ea33c45ac89f29eec56079baa7e54beccfc2b548
황인준 <os2864@naver.com>
2026-04-10 01:25:57 +0900

    fix: 임대인 최종 승인 시 알림 url에 임대인 권한 추가

--- back-end/src/main/java/com/bangjwo/notification/domain/vo/NotificationMessage.java
+++ back-end/src/main/java/com/bangjwo/notification/domain/vo/NotificationMessage.java
@@ -20,12 +20,12 @@ public enum NotificationMessage {
 	TENANT_COMPLETED(
 		NotificationType.CONTRACT_STATUS,
 		"임차인이 계약서 확인을 완료했습니다. 임차인이 최종 승인까지 기다려주세요.",
-		"/seller-contract/%d/%d"
+		"/buyer-contract/%d/%d"
 	),
 	TENANT_SIGNED(
 		NotificationType.CONTRACT_STATUS,
 		"임차인이 서명을 완료했습니다. 최종 서명을 진행해주세요.",
-		"/final-sign/%d/%d"
+		"/final-sign/%d/%d?role=LANDLORD"
 	),
 	CONTRACT_COMPLETED(
 		NotificationType.CONTRACT_STATUS,
