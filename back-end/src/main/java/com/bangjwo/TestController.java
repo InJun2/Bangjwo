016e191900a26172578c3d00b86fa4caaaa9c005
황인준 <os2864@naver.com>
2026-09-03 14:39:40 +0900

    chore: 테스트 컨트롤러 스웨거 문서화 주석 처리

--- back-end/src/main/java/com/bangjwo/TestController.java
+++ back-end/src/main/java/com/bangjwo/TestController.java
@@ -1,24 +1,24 @@
-package com.bangjwo;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.bangjwo.auth.resolver.MemberHeader;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-
-@RestController
-public class TestController {
-	@GetMapping
-	public String test() {
-		return "bangjwo-service (테스트)";
-	}
-
-	@Operation(summary = "테스트 컨트롤러", security = @SecurityRequirement(name = "JWT"))
-	@GetMapping("/test")
-	public ResponseEntity<String> test2(@MemberHeader Long memberId) {
-		return ResponseEntity.ok("Current memberId: " + memberId);
-	}
-}
+// package com.bangjwo;
+//
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
+//
+// import com.bangjwo.auth.resolver.MemberHeader;
+//
+// import io.swagger.v3.oas.annotations.Operation;
+// import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+//
+// @RestController
+// public class TestController {
+// 	@GetMapping
+// 	public String test() {
+// 		return "bangjwo-service (테스트)";
+// 	}
+//
+// 	@Operation(summary = "테스트 컨트롤러", security = @SecurityRequirement(name = "JWT"))
+// 	@GetMapping("/test")
+// 	public ResponseEntity<String> test2(@MemberHeader Long memberId) {
+// 		return ResponseEntity.ok("Current memberId: " + memberId);
+// 	}
+// }
